30fec1e373cfd6781ec09ec15575a6d16ff4ad3d
markggg02 <guanmaoye02@gmail.com>
2025-10-23 16:17:33 +0200

    add main file

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import preprocess
+import train_autoencoder
 
 if __name__ == "__main__":
-    preprocess.preprocess_all_data()
+    # preprocess.preprocess_all_data()
+    train_autoencoder.train_lstm_ae("mad_z_win200_s100")
