16da03cde8c70ed06c50907997a584d3f2ea0430
NinjaZny <280304803@qq.com>
2025-11-15 13:55:39 +0100

    add evaluation1

--- src/main.py
+++ src/main.py
@@ -1,18 +1,20 @@
 import preprocess
 import train_autoencoder
-import evaluate
-import cross_validation
+#import evaluate
+#import cross_validation
+import evaluation1
 
 if __name__ == "__main__":
     # When you change this pipline you also need to change the param described in preprocess.py
-    pipeline = "none_minmax_butterworth_none_Slidingwindow_"
+    pipeline = "IQR_minmax_butterworth_none_Slidingwindow_"
 
 
     # You can ignore the training since the models has been trained with all Pipelines,
     # but always run the Preprocess.py before evaluation
-    preprocess.preprocess_all_data()
-    #train_autoencoder.train_lstm_ae(pipeline)
-    evaluate.evaluate_pipeline(pipeline)
+    #preprocess.preprocess_all_data()
+    #train_autoencoder.train_lstm_ae_with_cv(pipeline)
+    evaluation1.evaluate_binary_detection()
+    #evaluate.evaluate_pipeline(pipeline)
 
 
     # cross-validation script to run Preprocess->Train->Evaluate on all the possible pipelines
